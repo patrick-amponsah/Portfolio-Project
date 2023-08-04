9b828e7e5482c409fb18accd12ea4ee598828294
Patrick Amponsah <esselltricky@outlook.com>
2023-08-04 05:46:22 -0700

    Update Local.Storage

--- LocalStorage.js
+++ LocalStorage.js
@@ -1,15 +1,30 @@
 const formString = document.querySelector('.contact-me-form-button');
+
 if (formString) {
-  formString.addEventListener('click', () => {
-    const information = {
-      name: document.getElementById('name').value,
-      email: document.getElementById('email').value,
-      message: document.getElementById('message').value,
-    };
-    localStorage.setItem('formString', JSON.stringify(information));
-  });
+  // Get references to the form elements
+  const nameInput = document.getElementById('name');
+  const emailInput = document.getElementById('email');
+  const messageInput = document.getElementById('message');
+
+  // Add input event listeners to the form elements
+  nameInput.addEventListener('input', saveFormToLocalStorage);
+  emailInput.addEventListener('input', saveFormToLocalStorage);
+  messageInput.addEventListener('input', saveFormToLocalStorage);
+
+  // Load and populate the form fields from localStorage if available
+  const savedForm = JSON.parse(localStorage.getItem('formString'));
+  if (savedForm) {
+    nameInput.value = savedForm.name || '';
+    emailInput.value = savedForm.email || '';
+    messageInput.value = savedForm.message || '';
+  }
 }
-const formObj = JSON.parse(localStorage.getItem('formString'));
-document.getElementById('name').value = formObj.name;
-document.getElementById('email').value = formObj.email;
-document.getElementById('message').value = formObj.message;
+
+function saveFormToLocalStorage() {
+  const information = {
+    name: document.getElementById('name').value,
+    email: document.getElementById('email').value,
+    message: document.getElementById('message').value,
+  };
+  localStorage.setItem('formString', JSON.stringify(information));
+}
